22e67b6b5a1473c97cb5ca350c6b8066c2eef605
cristian <epicstorm19@gmail.com>
2017-11-10 15:09:10 -0300

    first commit

---
+++ src/components/containers/index.js
@@ -0,0 +1,15 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+	Export your container components here. The Users
+	container is just an example and you will likely
+	remove it in favor of your own containers. 
+* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*/
+
+import Users from './Users'
+import AutomatedEmailCreator from './AutomatedEmailCreator'
+
+export {
+
+	Users, AutomatedEmailCreator
+	
+}
